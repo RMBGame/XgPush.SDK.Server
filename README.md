87bb69c8fdcabfdf4f1eed7d175c48aabe6a3b31
Aigio Liu <lysoft.pw@foxmail.com>
2018-12-22 11:36:24 +0800

    update README

--- README.md
+++ README.md
@@ -2,3 +2,207 @@
 [![NuGet version (XgPush.SDK.Server)](https://img.shields.io/nuget/v/XgPush.SDK.Server.svg)](https://www.nuget.org/packages/XgPush.SDK.Server/)
 
 XgPush Server SDK third party (unofficial) 
+
+## 快速开始
+
+* Package Manager
+
+```
+Install-Package XgPush.SDK.Server -Version 1.2.0-preview.812.compat.1.1.9
+```
+
+* .NET CLI
+
+```
+dotnet add package XgPush.SDK.Server --version 1.2.0-preview.812.compat.1.1.9
+```
+
+### 注意事项
+
+⚠ 当前包具有 [SemVer 2.0.0](https://docs.microsoft.com/zh-cn/nuget/reference/package-versioning#semantic-versioning-200) 仅可用于以下 NuGet 客户端：
+
+* NuGet 4.3.0
+* Visual Studio 2017 15.3 +
+* 使用 Visual Studio 2015 [NuGet VSIX v3.6.0](https://dist.nuget.org/visualstudio-2015-vsix/latest/NuGet.Tools.vsix)
+* dotnet
+    * dotnetcore.exe (.NET SDK 2.0.0+)
+
+第三方客户端：
+
+* JetBrains 的 Rider
+* Paket 依存版本 5.0 +
+
+### 初始化SDK
+
+#### AspNetCore 1.x/2.x
+
+[YourProject\Program.cs](/samples/aspnetcore21(netcoreapp21)/Program.cs)
+```
+public static void Main(string[] args){
+    + XgPush.SDK.Server.XingePushClient.InitHttpClientCompatDefaultMagic();
+}
+```
+
+#### AspNet
+
+[YourProject\Global.asax.cs](/samples/aspnetmvc5(net45)/Global.asax.cs)
+```
+protected void Application_Start(){
+    + XgPush.SDK.Server.XingePushClient.InitHttpClientCompatDefaultMagic();
+}
+```
+
+## 支持的目标框架
+
+* .NET Standard 1.3 **(不支持V2版本接口)**
+* .NET Standard 2.0
+* .NET Framework 4.0
+* .NET Framework 4.5
+
+## 示例
+
+* Push API v3
+    * 全量推送
+    * 标签推送
+    * 单设备推送
+    * 设备列表推送
+    * 单账号推送
+    * 账号列表推送
+* Rest API v2
+    * 全量推送
+    * 群推送
+    * 单推
+    * 查询消息状态
+    * 取消推送
+    * 批量新增标签
+    * 批量删除标签
+    * 查询全部标签
+    * 查询单个指定设备的标签
+    * 查询单个指定标签的Token总数
+    * 查询单个账号关联的设备
+    * 删除单个账号关联的单个设备Token
+    * 删除单个账号关联的全部设备Token
+    * 查询应用覆盖的设备Token总数
+    * 查询指定设备Token的注册状态
+
+[SampleController](/samples/aspnetcore21(netcoreapp21)/Controllers/SampleController.cs)
+
+### [Push API v3](https://xg.qq.com/docs/server_api/v3/push_api_v3.html)
+
+#### 全量推送
+
+```
+var msg_android = new Message
+{
+    Title = "your title",
+    Content = "your content",
+};
+
+var req_push_all = new PushRequest<Message>.All
+{
+    Message = msg_android,
+};
+
+var rsp_push_all = await pushClient.v3.Push(req_push_all);
+```
+
+#### 标签推送
+
+```
+var tags = new[] { "tag1", "tag2", "tag3" };
+
+var req_push_tag = new PushRequest<Message>.Tag
+{
+    Message = msg_android,
+    Value = new TagList(tags)
+    {
+        Operators = Operator.OR // Operator.AND
+    },
+};
+
+var rsp_push_tag = await pushClient.v3.Push(req_push_tag);
+```
+
+#### 单设备推送
+
+```
+var req_push_token = new PushRequest<Message>.Token
+{
+    Message = msg_android,
+    Value = "token1",
+};
+
+var rsp_push_token = await pushClient.v3.Push(req_push_token);
+```
+
+#### 设备列表推送
+
+```
+var tokens = new[] { "token1", "token2", "token3" };
+
+var req_push_tokens = new PushRequest<Message>.TokenList
+{
+    Message = msg_android,
+    Values = tokens,
+};
+
+var rsp_push_tokens = await pushClient.v3.Push(req_push_tokens);
+```
+
+#### 单账号推送
+
+```
+var req_push_account = new PushRequest<Message>.Account
+{
+    Message = msg_android,
+    Value = "account1",
+    Type = 0, //  往单个账号的最新的device上推送信息
+    // AccountType = AccountType.PhoneNumber, // 账号类型(可选)
+};
+
+var rsp_push_account = await pushClient.v3.Push(req_push_account);
+```
+
+#### 账号列表推送
+
+```
+var accounts = new[] { "account1", "account2", "account3" };
+
+var req_push_accounts = new PushRequest<Message>.AccountList
+{
+    Message = msg_android,
+    Values = accounts,
+    //PushId = "123",
+};
+
+var rsp_push_accounts = await pushClient.v3.Push(req_push_accounts);
+```
+
+### [Rest API v2](https://xg.qq.com/docs/server_api/v2/rest.html)
+
+<!--
+
+#### 全量推送
+
+```
+var req_push_all_v2 = await pushClient.PushAllDevice(msg_android);
+```
+
+#### 
+
+-->
+
+#### 查询消息状态
+
+```
+var pushIds = new[] { "push_id_1", "push_id_2" };
+
+var req_query_push_status = await pushClient.QueryPushStatus(pushIds);
+```
+
+#### 取消推送
+
+
+```
+var req_cancel_push = await pushClient.CancelTimingPush("push_id_1");
+```
---
+++ samples/aspnetcore21(netcoreapp21)/Controllers/SampleController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XgPush.SDK.Server;
+
+namespace Sample.XgPush.SDK.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleController : Controller
+    {
+        private readonly XingePushClient pushClient;
+
+        public SampleController(XingePushClient pushClient) => this.pushClient = pushClient;
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            Stop();
+
+            dynamic result = null;
+
+            var msg_android = new Message
+            {
+                Title = "your title",
+                Content = "your content",
+            };
+
+            var msg_iOS = new MessageiOS
+            {
+                AlertStr = "your content",
+                Badge = 1,
+                Environment = iOSEnvironment.Development,
+            };
+
+            #region Push API v3
+
+            // 全量推送
+
+            var req_push_all = new PushRequest<Message>.All
+            {
+                Message = msg_android,
+            };
+
+            var rsp_push_all = await pushClient.v3.Push(req_push_all);
+
+            result.rsp_push_all = rsp_push_all;
+
+            // 标签推送
+
+            var tags = new[] { "tag1", "tag2", "tag3" };
+
+            var req_push_tag = new PushRequest<Message>.Tag
+            {
+                Message = msg_android,
+                Value = new TagList(tags)
+                {
+                    Operators = Operator.OR // Operator.AND
+                },
+            };
+
+            var rsp_push_tag = await pushClient.v3.Push(req_push_tag);
+
+            result.rsp_push_tag = rsp_push_tag;
+
+            // 单设备推送
+
+            var req_push_token = new PushRequest<Message>.Token
+            {
+                Message = msg_android,
+                Value = "token1",
+            };
+
+            var rsp_push_token = await pushClient.v3.Push(req_push_token);
+
+            result.rsp_push_token = rsp_push_token;
+
+            // 设备列表推送
+
+            var tokens = new[] { "token1", "token2", "token3" };
+
+            var req_push_tokens = new PushRequest<Message>.TokenList
+            {
+                Message = msg_android,
+                Values = tokens,
+            };
+
+            var rsp_push_tokens = await pushClient.v3.Push(req_push_tokens);
+
+            result.rsp_push_tokens = rsp_push_tokens;
+
+            // 单账号推送
+
+            var req_push_account = new PushRequest<Message>.Account
+            {
+                Message = msg_android,
+                Value = "account1",
+                Type = 0, //  往单个账号的最新的device上推送信息
+                // AccountType = AccountType.PhoneNumber, // 账号类型(可选)
+            };
+
+            var rsp_push_account = await pushClient.v3.Push(req_push_account);
+
+            result.rsp_push_account = rsp_push_account;
+
+            // 账号列表推送
+
+            var accounts = new[] { "account1", "account2", "account3" };
+
+            var req_push_accounts = new PushRequest<Message>.AccountList
+            {
+                Message = msg_android,
+                Values = accounts,
+                //PushId = "123",
+            };
+
+            var rsp_push_accounts = await pushClient.v3.Push(req_push_accounts);
+
+            result.rsp_push_accounts = rsp_push_accounts;
+
+            #endregion
+
+            #region Rest API v2
+
+            // 全量推送
+
+            var req_push_all_v2 = await pushClient.PushAllDevice(msg_android);
+            result.req_push_all_v2 = req_push_all_v2;
+
+            // 查询消息状态
+
+            var pushIds = new[] { "push_id_1", "push_id_2" };
+
+            var req_query_push_status = await pushClient.QueryPushStatus(pushIds);
+
+            result.req_query_push_status = req_query_push_status;
+
+            // 取消推送
+
+            var req_cancel_push = await pushClient.CancelTimingPush("push_id_1");
+
+            #endregion
+
+            return Ok(result);
+        }
+
+        [NonAction]
+        private void Stop() => throw new Exception();
+    }
+}
